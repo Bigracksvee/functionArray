abbe7a2d825b48a08d5b087587fd0bdc97714d8d
Vera <veraopoku100@gmail.com>
2022-07-26 08:55:52 -0400

    arrayFunction

---
+++ app.js
@@ -0,0 +1,21 @@
+const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j",];
+
+function left([], b){}
+
+left.push("a");
+
+console.log(left);
+
+
+function right([c]){}
+
+    right.splice("","")
+    return [c];
+    console.log(right);
+
+
+function up([d]){}
+
+    up.slice(f,g)
+    return ;
+    console.log(up);
